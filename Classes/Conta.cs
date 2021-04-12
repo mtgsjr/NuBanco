96537f867fadc2fe9ff0b0f90d85b88356451f58
Marcos Tulio Gomes da Silva Junior <mtgsjr@hotmail.com>
2021-04-11 21:57:04 -0300

    primeira postagem

---
+++ Classes/Conta.cs
@@ -0,0 +1,7 @@
+namespace DIO.Bank
+{
+    internal class Conta
+    {
+        public string Nome {set; get;}
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DIO.Bank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Conta minhaconta = new Conta();
+            minhaconta.Nome = "Marcos Tulio";
+            Console.WriteLine("Nome: " + minhaconta.Nome);
+        }
+    }
+}
